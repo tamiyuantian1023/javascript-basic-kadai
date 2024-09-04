d58ec0f69e109e6c2efef353e071970e0bc354d4
tamiyuantian1023 <tamiyuantian1023@gmail.com>
2024-09-04 21:51:57 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,17 @@
 let num = Math.floor(Math.random() * 15) + 1;
  
 console.log(num);
+
+if ((num % 3) === 0 && (num % 5) === 0) {
+  console.log('3と5の倍数です');
+}
  
-if (num === 3) {
+else if ((num % 3) === 0 ){
    console.log('3の倍数です');
 }
 
-else if (num === 5) {
+else if ((num % 5) === 0) {
   console.log('5の倍数です');
-}
-
-else if ((num % 3) === 0 && (num % 5) === 0) {
-  console.log('3と5の倍数です');
 }
 
 else {
